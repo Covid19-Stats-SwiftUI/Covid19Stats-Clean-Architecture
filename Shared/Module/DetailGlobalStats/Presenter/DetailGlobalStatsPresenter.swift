602c8d690d88b4b4f5bb509c2d95c9b43403026c
Ari Supriatna <ari@myboost.id>
2020-12-08 15:15:20 +0700

    Implement DetailGlobalStatsPresenter

--- Shared/Module/DetailGlobalStats/Presenter/DetailGlobalStatsPresenter.swift
+++ Shared/Module/DetailGlobalStats/Presenter/DetailGlobalStatsPresenter.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+import Combine
+
+class DetailGlobalStatsPresenter: ObservableObject {
+  
+  private let globalStatsUseCase: DetailGlobalStatsUseCase
+  private var cancellables: Set<AnyCancellable> = []
+  
+  @Published var isLoadingState = false
+  @Published var stats: [CountryCaseStatsModel] = []
+  @Published var errorMessage: String = ""
+  
+  init(globalStatsUseCase: DetailGlobalStatsUseCase) {
+    self.globalStatsUseCase = globalStatsUseCase
+  }
+  
+  func getDetailGlobalStats(by caseType: Endpoints.CaseType) {
+    isLoadingState = true
+    
+    globalStatsUseCase.getDetailGlobalStats(by: caseType)
+      .receive(on: RunLoop.main)
+      .sink(receiveCompletion: { error in
+        switch error {
+        case .failure:
+          self.isLoadingState = false
+          self.errorMessage = String(describing: error)
+          debugPrint("errorMessage: \(self.errorMessage)")
+        case .finished:
+          self.isLoadingState = false
+          debugPrint("isLoading: \(self.isLoadingState)")
+        }
+      }, receiveValue: { result in
+        self.stats = result
+        debugPrint("Result: \(self.stats)")
+      })
+      .store(in: &cancellables)
+  }
+  
+}
