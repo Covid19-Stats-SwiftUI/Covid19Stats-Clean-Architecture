7c865548f5c825727a0be9d63e03449b60a086b3
Ari Supriatna <ari@myboost.id>
2020-12-08 15:11:19 +0700

    Implement DetailGlobalStatsInteractor

--- Shared/Core/Domain/Use Case/DetailGlobalStatsInteractor.swift
+++ Shared/Core/Domain/Use Case/DetailGlobalStatsInteractor.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import Combine
+
+protocol DetailGlobalStatsUseCase {
+
+  func getDetailGlobalStats(by caseType: Endpoints.CaseType) -> AnyPublisher<[CountryCaseStatsModel], Covid19APIError>
+
+}
+
+class DetailGlobalStatsInteractor: DetailGlobalStatsUseCase {
+
+  private let repository: CovidRepositoryProtocol
+
+  required init(repository: CovidRepositoryProtocol) {
+    self.repository = repository
+  }
+
+  func getDetailGlobalStats(by caseType: Endpoints.CaseType) -> AnyPublisher<[CountryCaseStatsModel], Covid19APIError> {
+    return repository.getCountryCaseStats(by: caseType)
+  }
+
+}
