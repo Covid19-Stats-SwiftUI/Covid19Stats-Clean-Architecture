6f7bb086adabb18372ff957d97796f0b8826b0b4
Ari Supriatna <ari@myboost.id>
2020-12-08 15:14:34 +0700

    Implement PreventionView

--- Shared/Core/Utils/View/PreventionView.swift
+++ Shared/Core/Utils/View/PreventionView.swift
@@ -8,13 +8,41 @@
 import SwiftUI
 
 struct PreventionView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  var body: some View {
+    HStack {
+      VStack {
+        Image("close-contact")
+        Text("Avoid close contact")
+          .font(.system(size: 16, weight: .medium))
+          .multilineTextAlignment(.center)
+      }
+      .frame(width: 100, height: 140)
+      
+      Spacer()
+      
+      VStack {
+        Image("clean-hands")
+        Text("Clean your hands often")
+          .font(.system(size: 16, weight: .medium))
+          .multilineTextAlignment(.center)
+      }
+      .frame(width: 100, height: 140)
+      
+      Spacer()
+      
+      VStack {
+        Image("wear-face-mask")
+        Text("Wear a facemask")
+          .font(.system(size: 16, weight: .medium))
+          .multilineTextAlignment(.center)
+      }
+      .frame(width: 100, height: 140)
     }
+  }
 }
 
 struct PreventionView_Previews: PreviewProvider {
-    static var previews: some View {
-        PreventionView()
-    }
+  static var previews: some View {
+    PreventionView()
+  }
 }
