2e4315a3f003d3dc25be4c4caf0b4a809db04649
Ari Supriatna <ari@myboost.id>
2020-12-08 15:07:18 +0700

    implement SearchBar View

--- Shared/Core/Utils/View/SearchBar.swift
+++ Shared/Core/Utils/View/SearchBar.swift
@@ -7,14 +7,57 @@
 
 import SwiftUI
 
-struct SearchBar: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct SearchBar<Content: View>: UIViewControllerRepresentable {
+  
+  @Binding var text: String
+  var content: () -> Content
+  
+  func makeUIViewController(context: Context) -> UINavigationController {
+    let navigationController = UINavigationController(rootViewController: context.coordinator.rootViewController)
+    navigationController.navigationBar.prefersLargeTitles = true
+    
+    context.coordinator.searchController.searchBar.delegate = context.coordinator
+    
+    return navigationController
+  }
+  
+  func updateUIViewController(_ uiViewController: UINavigationController, context: Context) {
+    context.coordinator.update(content: content())
+  }
+  
+  func makeCoordinator() -> Coordinator {
+    Coordinator(content: content(), searchText: $text)
+  }
+  
+  class Coordinator: NSObject, UISearchBarDelegate {
+    @Binding var text: String
+    let rootViewController: UIHostingController<Content>
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    init(content: Content, searchText: Binding<String>) {
+      rootViewController = UIHostingController(rootView: content)
+      searchController.searchBar.autocapitalizationType = .none
+      searchController.obscuresBackgroundDuringPresentation = false
+      rootViewController.navigationItem.searchController = searchController
+      
+      _text = searchText
     }
-}
-
-struct SearchBar_Previews: PreviewProvider {
-    static var previews: some View {
-        SearchBar()
+    
+    func update(content: Content) {
+      rootViewController.rootView = content
+      rootViewController.view.setNeedsDisplay()
     }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+      text = searchText
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+      searchBar.resignFirstResponder()
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+      text = ""
+    }
+  }
 }
