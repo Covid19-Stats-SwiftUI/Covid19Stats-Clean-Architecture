2ee3d7f6783584e2ab0c0e4d8f69dbe438a29623
Ari Supriatna <ari@myboost.id>
2020-12-08 15:10:40 +0700

    Refactor CovidRepository

--- Shared/Core/Data/CovidRepository.swift
+++ Shared/Core/Data/CovidRepository.swift
@@ -11,11 +11,9 @@ import Combine
 protocol CovidRepositoryProtocol {
   
   func getGlobalCaseStats() -> AnyPublisher<GlobalCaseStatsModel, Covid19APIError>
-  func getCountryCaseStatsConfirmed(by caseType: Endpoints.CaseType) -> AnyPublisher<[CountryCaseStatsModel], Covid19APIError>
-  func getCountryCaseStatsRecovered(by caseType: Endpoints.CaseType) -> AnyPublisher<[CountryCaseStatsModel], Covid19APIError>
-  func getCountryCaseStatsDeath(by caseType: Endpoints.CaseType) -> AnyPublisher<[CountryCaseStatsModel], Covid19APIError>
   func getCountries() -> AnyPublisher<[CountryModel], Covid19APIError>
   func getStatsByCountry(country name: String) -> AnyPublisher<GlobalCaseStatsModel, Covid19APIError>
+  func getCountryCaseStats(by caseType: Endpoints.CaseType) -> AnyPublisher<[CountryCaseStatsModel], Covid19APIError>
   
 }
 
@@ -42,24 +40,6 @@ extension CovidRepository: CovidRepositoryProtocol {
       .eraseToAnyPublisher()
   }
   
-  func getCountryCaseStatsConfirmed(by caseType: Endpoints.CaseType) -> AnyPublisher<[CountryCaseStatsModel], Covid19APIError> {
-    self.remote.getCountryCaseStats(by: .confirmed)
-      .map  { CountryCaseStatsMapper.mapCountryCaseStatsResponseToDomains(input: $0) }
-      .eraseToAnyPublisher()
-  }
-  
-  func getCountryCaseStatsRecovered(by caseType: Endpoints.CaseType) -> AnyPublisher<[CountryCaseStatsModel], Covid19APIError> {
-    self.remote.getCountryCaseStats(by: .recovered)
-      .map  { CountryCaseStatsMapper.mapCountryCaseStatsResponseToDomains(input: $0) }
-      .eraseToAnyPublisher()
-  }
-  
-  func getCountryCaseStatsDeath(by caseType: Endpoints.CaseType) -> AnyPublisher<[CountryCaseStatsModel], Covid19APIError> {
-    self.remote.getCountryCaseStats(by: .deaths)
-      .map  { CountryCaseStatsMapper.mapCountryCaseStatsResponseToDomains(input: $0) }
-      .eraseToAnyPublisher()
-  }
-  
   func getCountries() -> AnyPublisher<[CountryModel], Covid19APIError> {
     self.remote.getCountries()
       .map { CountryMapper.mapCountryResponseToDomains(input: $0) }
@@ -69,6 +49,12 @@ extension CovidRepository: CovidRepositoryProtocol {
   func getStatsByCountry(country name: String) -> AnyPublisher<GlobalCaseStatsModel, Covid19APIError> {
     self.remote.getStatsByCountry(country: name)
       .map { GlobalCaseStatsMapper.mapGlobalCaseStatsResponseToDomains(confirmed: $0.confirmed, recovered: $0.recovered, deaths: $0.deaths) }
+      .eraseToAnyPublisher()
+  }
+  
+  func getCountryCaseStats(by caseType: Endpoints.CaseType) -> AnyPublisher<[CountryCaseStatsModel], Covid19APIError> {
+    self.remote.getCountryCaseStats(by: caseType)
+      .map { CountryCaseStatsMapper.mapCountryCaseStatsResponseToDomains(input: $0) }
       .eraseToAnyPublisher()
   }
   
