d8855c33cbbc8c8f2e3bd5ca0bc2fa3a30fbdcf7
Ari Supriatna <ari@myboost.id>
2020-12-08 15:11:58 +0700

    Implement DetailGlobalStatsView

--- Shared/Module/DetailGlobalStats/View/DetailGlobalStatsView.swift
+++ Shared/Module/DetailGlobalStats/View/DetailGlobalStatsView.swift
@@ -8,13 +8,68 @@
 import SwiftUI
 
 struct DetailGlobalStatsView: View {
+  
+  @ObservedObject var presenter: DetailGlobalStatsPresenter
+  var caseType: Endpoints.CaseType
+  
+  var titleHeader: String {
+    switch caseType {
+    case .confirmed:
+      return "Confirmed Stats"
+    case .recovered:
+      return "Recovered Stats"
+    case .deaths:
+      return "Deaths Stats"
+    }
+  }
+  
   var body: some View {
-    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    ZStack {
+      if presenter.isLoadingState {
+        ProgressView()
+      } else {
+        List {
+          ForEach(presenter.stats.suffix(20)) { result in
+            DisplayName(stats: result, caseType: caseType)
+          }
+        }
+        .listStyle(InsetGroupedListStyle())
+      }
+    }
+    .navigationTitle(titleHeader)
+    .onAppear {
+      if self.presenter.stats.count == 0 {
+        self.presenter.getDetailGlobalStats(by: caseType)
+      }
+    }
   }
 }
 
-struct DetailGlobalStatsView_Previews: PreviewProvider {
-  static var previews: some View {
-    DetailGlobalStatsView()
+struct DisplayName: View {
+  
+  var stats: CountryCaseStatsModel
+  var caseType: Endpoints.CaseType
+  
+  var totalCase: Int {
+    switch caseType {
+    case .confirmed:
+      return stats.confirmed
+    case .recovered:
+      return stats.recovered
+    case .deaths:
+      return stats.deaths
+    }
   }
+  
+  @ViewBuilder
+  var body: some View {
+    Text("\(stats.displayName): \(totalCase)")
+  }
+  
 }
+
+//struct DetailGlobalStatsView_Previews: PreviewProvider {
+//  static var previews: some View {
+//    DetailGlobalStatsView()
+//  }
+//}
