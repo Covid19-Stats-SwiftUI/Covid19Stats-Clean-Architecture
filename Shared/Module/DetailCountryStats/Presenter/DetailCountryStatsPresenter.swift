c38d5bf6d2d6e53cd51938db0951b82836bd5abf
Ari Supriatna <ari@myboost.id>
2020-12-21 13:37:34 +0700

    refactor(DetailCountryStats): implementing view and presenter for DetailCountryStats

--- Shared/Module/DetailCountryStats/Presenter/DetailCountryStatsPresenter.swift
+++ Shared/Module/DetailCountryStats/Presenter/DetailCountryStatsPresenter.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import Combine
+
+class DetailCountryStatsPresenter: ObservableObject {
+  
+  private var cancellables: Set<AnyCancellable> = []
+  private let remote: CovidNetworkDataSource = CovidNetworkDataSource.sharedInstance
+  
+  @Published var isLoadingState = false
+  @Published var errorMessage: String = ""
+  @Published var stats: GlobalCaseStatsModel?
+  
+  func getStatsByCountry(for countryName: String) {
+    isLoadingState = true
+    
+    self.remote.getStatsByCountry(country: countryName)
+      .map { GlobalCaseStatsMapper.mapGlobalCaseStatsResponseToDomains(confirmed: $0.confirmed, recovered: $0.recovered, deaths: $0.deaths) }
+      .receive(on: RunLoop.main)
+      .sink(receiveCompletion: { error in
+        switch error {
+        case .failure:
+          self.isLoadingState = false
+          self.errorMessage = String(describing: error)
+        case .finished:
+          self.isLoadingState = false
+        }
+      }, receiveValue: { result in
+        self.stats = result
+      })
+      .store(in: &cancellables)
+    
+  }
+  
+}
--- Shared/Module/DetailCountryStats/View/DetailCountryStatsView.swift
+++ Shared/Module/DetailCountryStats/View/DetailCountryStatsView.swift
@@ -8,13 +8,56 @@
 import SwiftUI
 
 struct DetailCountryStatsView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  @StateObject var presenter = DetailCountryStatsPresenter()
+  var country: CountryModel
+  
+  var body: some View {
+    ZStack {
+      if let stats = presenter.stats {
+        List {
+          Section(header: Text("Stats of \(country.displayName)")) {
+            VStack(alignment: .leading, spacing: 16) {
+              Text("Confirmed")
+                .font(.title3)
+                .bold()
+              
+              Text(stats.confirmedText)
+                .fontWeight(.semibold)
+                .foregroundColor(Color("confirmedColor"))
+              
+              Text("Recovered")
+                .font(.title3)
+                .bold()
+              
+              Text(stats.recoveredText)
+                .fontWeight(.semibold)
+                .foregroundColor(Color("recoveredColor"))
+              
+              Text("Deaths")
+                .font(.title3)
+                .bold()
+              
+              Text(stats.deathText)
+                .fontWeight(.semibold)
+                .foregroundColor(Color("deathColor"))
+            }
+            .padding(.vertical)
+          }
+        }
+        .listStyle(InsetGroupedListStyle())
+      } else {
+        ProgressView()
+      }
     }
+    .navigationBarTitle(Text(country.displayName), displayMode: .large)
+    .onAppear {
+      self.presenter.getStatsByCountry(for: country.name)
+    }
+  }
 }
 
-struct DetailCountryStatsView_Previews: PreviewProvider {
-    static var previews: some View {
-        DetailCountryStatsView()
-    }
-}
+//struct DetailCountryStatsView_Previews: PreviewProvider {
+//  static var previews: some View {
+//    DetailCountryStatsView()
+//  }
+//}
