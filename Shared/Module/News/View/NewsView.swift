ad886fbd52caed6df1bc8a6b56b804687d63de29
Ari Supriatna <ari@myboost.id>
2020-12-21 13:47:41 +0700

    refactor(NewsView): implementing In-App Purchase

--- Shared/Module/News/View/NewsView.swift
+++ Shared/Module/News/View/NewsView.swift
@@ -10,29 +10,57 @@ import SwiftUI
 struct NewsView: View {
   
   @ObservedObject var presenter: NewsPresenter
-  @State var isShowSafari: Bool = false
-  @State var newsUrl: String = "https://google.com"
+  @StateObject var storeManager: StoreManager
+  @State var newsUrl: NewsModel?
+  
+  var isPurchaseProduct: Bool {
+    return UserDefaults.standard.bool(forKey: storeManager.myProducts[0].productIdentifier)
+  }
   
   var body: some View {
     ZStack {
       if let news = presenter.news {
-        List(news) { result in
+        List(isPurchaseProduct ? news : news.suffix(3)) { result in
           NewsRowView(news: result)
             .padding(.vertical, 8)
             .onTapGesture {
-              self.isShowSafari = true
-              self.newsUrl = result.url
+              self.newsUrl = result
             }
         }
         .listStyle(InsetGroupedListStyle())
-        .sheet(isPresented: $isShowSafari) {
-          SafariView(url: URL(string: self.newsUrl)!)
+        .sheet(item: self.$newsUrl) { item in
+          SafariView(url: URL(string: item.url)!)
         }
+        .onOpenURL(perform: { url in
+          guard let queryItems = URLComponents(url: url, resolvingAgainstBaseURL: false)?.queryItems else {
+            self.newsUrl = nil
+            return
+          }
+          
+          var dict = [String: String]()
+          queryItems.forEach { (item) in
+            dict[item.name] = item.value
+          }
+          
+          guard let url = dict["url"] else {
+            self.newsUrl = nil
+            return
+          }
+          
+          self.newsUrl = NewsModel(author: "-", title: "-", description: "-", url: url, urlToImage: "-", content: "-", source: SourceModel(name: "-"))
+        })
       } else {
         ProgressView()
       }
     }
-    .navigationTitle("News")
+    .toolbar(content: {
+      ToolbarItem(placement: .navigationBarTrailing) {
+        Button(action: !isPurchaseProduct ? { self.storeManager.purchaseProduct(product: storeManager.myProducts[0]) } : {}) {
+          Text(!isPurchaseProduct ? "Try Premium" : "")
+        }
+      }
+    })
+    .navigationTitle(isPurchaseProduct ? "News Premium" : "News")
     .onAppear {
       self.presenter.getNews()
     }
